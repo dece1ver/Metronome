3cf1943080b5a8ab083d81b94b8c5ac84ed7935e
Vladimir Gromov <v.dece1verr@hotmail.com>
2022-06-27 00:19:53 +0300

    сделал запоминание темпов (пока только до перезапуска, потом сделаю в локалсторадж)

---
+++ Metronome.WASM/Data/TempoInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Metronome.WASM.Data
+{
+    public class TempoInfo
+    {
+        public int Tempo { get; set; }
+        public string Name { get; set; }
+
+        public TempoInfo(int tempo, string name)
+        {
+            Tempo = tempo;
+            Name = name;
+        }
+    }
+}
